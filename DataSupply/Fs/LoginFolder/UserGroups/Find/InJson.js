7cd1940a21c8ae303d30b0d92de9f25493c8baea
keshavsoft <nknnkumar@yahoo.com>
2022-11-03 12:16:59 +0530

    keshav

--- DataSupply/Fs/LoginFolder/UserGroups/Find/InJson.js
+++ DataSupply/Fs/LoginFolder/UserGroups/Find/InJson.js
@@ -2,9 +2,9 @@ let _ = require("lodash");
 
 let CommonPullData = require("../PullData/FromJson");
 
-let StartFunc = async ({ inUserName, inPassword, inFirmName }) => {
+let StartFunc = async ({ inUserName, inPassWord, inFirmName }) => {
     let LocalUserName = inUserName;
-    let LocalPassword = inPassword;
+    let LocalPassword = inPassWord;
     let LocalFirmName = inFirmName;
 
     let LocalReturnData = { KTF: false, DirPath: "", FilePath: "", CreatedLog: {} };
@@ -12,20 +12,15 @@ let StartFunc = async ({ inUserName, inPassword, inFirmName }) => {
     let LocalFromPullData = await CommonPullData.AsAsync();
     let LocalUserKeyNeeded = _.findKey(LocalFromPullData, { "UserName": LocalUserName, "PassWord": LocalPassword });
 
-    console.log("LocalUserDataNeeded : ", LocalUserKeyNeeded);
-
     let LocalDataPK = _.findKey(LocalFromPullData[LocalUserKeyNeeded].ConnectedDatas, { "FirmName": LocalFirmName });
     LocalReturnData.kPK = LocalDataPK;
     LocalReturnData.KTF = true;
-
-    console.log("LocalFindConnectedDatas : ", LocalFindConnectedDatas);
 
     return await LocalReturnData;
 };
 
 let LocalMockFuncStartFunc = async () => {
     console.log("ssssssss : ", await StartFunc({ inUserName: "ABS", inPassword: "ABS", inFirmName: "ABS IMPEX-CHENNAI" }));
-
 };
 
 
