8947d8a1d5b35d63452ab272efec386d118ece6c
keshavsoft <nknnkumar@yahoo.com>
2022-11-28 15:22:12 +0530

    keshav

--- DataSupply/Fs/Config/Folders/Files/Items/PullData/FromDataFolder/AsTree/WithCounts.js
+++ DataSupply/Fs/Config/Folders/Files/Items/PullData/FromDataFolder/AsTree/WithCounts.js
@@ -8,7 +8,7 @@ let StartFunc = async ({ inDataPK, inFolderName, inFileNameWithExtension }) => {
         inFileNameWithExtension,
         inUserPK: LocalDataPK
     });
-   
+
     let LocalConfigData = await CommonFromConfig.FromFolderAndFile({
         inFolderName, inFileNameWithExtension,
         inUserPK: LocalDataPK
@@ -16,17 +16,19 @@ let StartFunc = async ({ inDataPK, inFolderName, inFileNameWithExtension }) => {
 
     let LocalReturnObject = {};
 
-    Object.entries(LocalJsonFileData).forEach(
-        ([key, value]) => {
-            if (key in LocalConfigData) {
-                LocalReturnObject[key] = {
-                    ItemName: key,
-                    RowCount: Object.keys(value).length,
-                    ScreenCount: Object.keys(LocalConfigData[key]).length
-                };
+    if (LocalConfigData !== undefined) {
+        Object.entries(LocalJsonFileData).forEach(
+            ([key, value]) => {
+                if (key in LocalConfigData) {
+                    LocalReturnObject[key] = {
+                        ItemName: key,
+                        RowCount: Object.keys(value).length,
+                        ScreenCount: Object.keys(LocalConfigData[key]).length
+                    };
 
-            };
-        });
+                };
+            });
+    };
 
     return await LocalReturnObject;
 };
@@ -37,8 +39,8 @@ let LocalMockFunc = async () => {
         inFolderName: "Masters",
         inFileNameWithExtension: "Products.json"
     });
-    
-     console.log("LocalData:", LocalData);
+
+    console.log("LocalData:", LocalData);
 };
 
 // LocalMockFunc().then(P => {
