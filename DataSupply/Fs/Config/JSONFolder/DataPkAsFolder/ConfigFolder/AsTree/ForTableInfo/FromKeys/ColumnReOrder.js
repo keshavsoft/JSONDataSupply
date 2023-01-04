48bbff5454a51b8d03b0361eeffa24181e79782c
Keshav <nknnkumar@yahoo.com>
2023-01-04 12:46:41 +0530

    keshav

--- DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableInfo/FromKeys/ColumnReOrder.js
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableInfo/FromKeys/ColumnReOrder.js
@@ -3,7 +3,7 @@ let CommonFromgetDirectories = require("../../../getDirectories");
 let _ = require("lodash");
 
 
-let AsObject = async ({ inDataPK }) => {
+let AsObject_4Jan2023 = async ({ inDataPK }) => {
     let LocalDataPK = inDataPK;
     let LocalReturnObject = {};
     LocalReturnObject.Folders = {};
@@ -63,9 +63,77 @@ let AsObject = async ({ inDataPK }) => {
 
     return await LocalReturnObject;
 };
+
+let AsObject = async ({ inDataPK }) => {
+    let LocalDataPK = inDataPK;
+    let LocalReturnObject = {};
+    LocalReturnObject.Folders = {};
+
+    let LocalArray = CommonFromgetDirectories.StartFunc({ inDataPK: LocalDataPK });
+
+    const result = await Promise.all(LocalArray.map(async (file) => {
+        let LoopInsideFile = await CommonFromUserFolder.AsObjects({
+            inFolderName: file,
+            inDataPK: LocalDataPK
+        });
+
+        return await {
+            FolderName: file,
+            Files: LoopInsideFile
+        };
+    }));
+
+    let LocalAltered = result.map(element => {
+        let LoopInsideFile = JSON.parse(JSON.stringify(element));
+        LoopInsideFile.Files = {};
+
+        Object.entries(element.Files).forEach(
+            ([FileKey, FileValue]) => {
+                LoopInsideFile.Files[FileKey] = JSON.parse(JSON.stringify(FileValue));
+
+                Object.entries(FileValue.Items).forEach(
+                    ([ItemKey, ItemValue]) => {
+                        LoopInsideFile.Files[FileKey].Items[ItemKey] = JSON.parse(JSON.stringify(ItemValue));
+
+                        Object.entries(ItemValue.Screens).forEach(
+                            ([ScreenKey, ScreenValue]) => {
+                                delete ScreenValue.TableColumnsObject;
+
+                                ScreenValue.ColumnReOrder = ScreenValue.TableInfo.ColumnReOrder;
+
+                                LoopInsideFile.Files[FileKey].Items[ItemKey].Screens[ScreenKey] = JSON.parse(JSON.stringify(ScreenValue));
+                                console.log("sssssss : ", Object.keys(ScreenValue));
+                                //LoopInsideFile.Files[FileKey].Items[ItemKey].Screens[ScreenKey].ColumnReOrder = ScreenValue.TableInfo.ColumnReOrder;
+
+                                // Object.entries(ScreenValue.TableColumnsObject).forEach(
+                                //     ([ColumnKey, ColumnValue]) => {
+                                //         LoopInsideFile.Files[FileKey].Items[ItemKey].Screens[ScreenKey].TableColumnsObject[ColumnKey] = {
+                                //             ColumnReOrder: ColumnValue.ColumnReOrder
+                                //         };
+                                //     }
+                                // );
+                            }
+                        );
+                    }
+                );
+            }
+        );
+
+        return LoopInsideFile;
+    });
+
+    //  console.log("result---------- : ", LocalAltered[0].Files.Accounts.Items.Accounts.Screens.Create.TableColumnsObject.pk);
+
+    LocalAltered.forEach(element => {
+        LocalReturnObject.Folders[element.FolderName] = element;
+    });
+
+    return await LocalReturnObject;
+};
+
 let LocalMockFunc = async () => {
     let LocalData = await AsObject({ inDataPK: 901 });
-      console.log("LocalData : ", LocalData);
+    console.log("LocalData : ", LocalData);
 };
 
 //LocalMockFunc().then();
