5265f20009128133f8fb2dd9c863d406317ba4a0
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-09 21:16:05 +0530

    SREENIVAS

--- DataSupply/Fs/Config/JSONFolder/UserFolders/PullFolders/getDirectoriesWithUserCredntials.js
+++ DataSupply/Fs/Config/JSONFolder/UserFolders/PullFolders/getDirectoriesWithUserCredntials.js
@@ -7,8 +7,6 @@ let StartFunc = () => {
     let LocalReturnData = { KTF: false, JSONObject: {}, CreatedLog: {} };
     let LocalFromCommongetDirectoriesArray = CommongetDirectories.StartFunc();
     let LocalLoginDetails = CommonLoginDetails.StartFunc();
-    console.log("LocalFromCommongetDirectoriesArray-", LocalLoginDetails);
-
 
     if (LocalLoginDetails.KTF === false) {
         LocalReturnData.KReason = LocalLoginDetails.KReason;
@@ -17,24 +15,15 @@ let StartFunc = () => {
     };
 
     LocalFromCommongetDirectoriesArray.forEach(element => {
-        LocalReturnData.JSONObject[element] = LocalLoginDetails.JsonData[element];
+        LocalReturnData.JSONObject[element] = {};
+        if (element in LocalLoginDetails.JsonData) {
+            LocalReturnData.JSONObject[element] = LocalLoginDetails.JsonData[element];
+
+        };
     });
     LocalReturnData.KTF = true;
-    console.log("LocalReturnData--", LocalReturnData);
     return LocalReturnData;
 
-    // let LocalDataPath = `${LocalFromCommonFromCheck.JSONFolderPath}`;
-    // let LocalReturnArray = fs.readdirSync(LocalDataPath).filter(function (file) {
-    //     return fs.statSync(LocalDataPath + '/' + file).isDirectory();
-    // });
-
-    // let LocalNumberArray = LocalReturnArray.map(element => parseInt(element));
-
-    // let LocalFilterArray = LocalNumberArray.filter(element => {
-    //     return isNaN(element) === false;
-    // });
-
-    // return LocalFilterArray;
 };
 
 let LocalMockFunc = () => {
@@ -42,6 +31,6 @@ let LocalMockFunc = () => {
     console.log("LocalData--", LocalData);
 };
 
-LocalMockFunc();
+// LocalMockFunc();
 
 module.exports = { StartFunc };
