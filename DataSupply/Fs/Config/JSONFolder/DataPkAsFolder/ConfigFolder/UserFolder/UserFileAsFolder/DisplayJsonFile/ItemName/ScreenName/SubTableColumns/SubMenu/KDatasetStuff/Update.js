2a2a8440f922c9bd05fdfdd872863d2fce628a0d
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-11 19:26:43 +0530

    SREENIVAS

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableColumns/SubMenu/KDatasetStuff/Update.js
@@ -0,0 +1,75 @@
+let _ = require("lodash");
+
+let CommonPullDataFromConfig = require("../../../../../PullData/AsJson");
+let CommonFromPushData = require("../../../../../PushData/FromFoldFile");
+
+
+let Update = async ({ DataPK, folderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson }) => {
+    console.log("BodyAsJson", BodyAsJson);
+    const LocalDataToUpdate = (({ Validate, DataListReverse, Type }) => ({ Validate, DataListReverse, Type }))(BodyAsJson);
+    let LocalinDataPK = DataPK;
+
+    let inJsonConfig = { inFolderName: folderName, inJsonFileName: FileName }
+    let LocalItemName = ItemName;
+    let LocalScreenName = ScreenName;
+    let LocalFindColumnObject;
+    let LocalFromUpdate;
+    let LocalReturnObject = { KTF: false };
+
+    let LocalFromPullData = await CommonPullDataFromConfig.FromJsonConfig({
+        inJsonConfig,
+        inDataPK: LocalinDataPK
+    });
+
+    let LocalNewData = JSON.parse(JSON.stringify(LocalFromPullData.JsonData));
+
+    if (LocalItemName in LocalNewData) {
+        if (LocalScreenName in LocalNewData[LocalItemName]) {
+            if ("SubTableColumns" in LocalNewData[LocalItemName][LocalScreenName]) {
+                if ("FinGrid" in LocalNewData[LocalItemName][LocalScreenName].SubTableColumns) {
+                    if ("TableColumns" in LocalNewData[LocalItemName][LocalScreenName].SubTableColumns.FinGrid) {
+
+                        LocalFindColumnObject = _.find(LocalNewData[LocalItemName][LocalScreenName].SubTableColumns.FinGrid.TableColumns, { DataAttribute });
+
+                        LocalFindColumnObject.KDatasetStuff.Validate = LocalDataToUpdate.Validate;
+                        LocalFindColumnObject.KDatasetStuff.Type = LocalDataToUpdate.Type;
+                        LocalFindColumnObject.KDatasetStuff.DataListReverse = LocalDataToUpdate.DataListReverse;
+
+                        LocalFromUpdate = await CommonFromPushData.StartFunc({
+                            inFolderName: folderName,
+                            inFileNameWithExtension: FileName,
+                            inDataPK: LocalinDataPK,
+                            inDataToUpdate: LocalNewData,
+                            inOriginalData: LocalFromPullData.JsonData
+                        });
+
+
+                        if (LocalFromUpdate.KTF) {
+                            LocalReturnObject.KTF = true;
+                        };
+
+                        return await LocalReturnObject;
+                    };
+                };
+            };
+           
+        };
+    };
+
+    return await LocalReturnObject;
+};
+
+// UpdateKeys({
+//     DataPK: 16, folderName: "Masters", FileName: "Customers.json", ItemName: "CustomersName", ScreenName: "Create",
+//     DataAttribute: "pk",
+//     BodyAsJson: {
+//         DisplayName: "ppppppppppp"
+//     }
+// }).then(p => {
+//     console.log("pppp : ", p);
+// });
+
+
+module.exports = {
+    Update
+};
---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableColumns/SubMenu/Widths/Update.js
@@ -0,0 +1,73 @@
+let _ = require("lodash");
+
+let CommonPullDataFromConfig = require("../../../../../PullData/AsJson");
+let CommonFromPushData = require("../../../../../PushData/FromFoldFile");
+
+
+let Update = async ({ DataPK, folderName, FileName, ItemName, ScreenName, DataAttribute, BodyAsJson }) => {
+    console.log("BodyAsJson", BodyAsJson);
+    const LocalDataToUpdate = (({ px }) => ({ px }))(BodyAsJson);
+    let LocalinDataPK = DataPK;
+
+    let inJsonConfig = { inFolderName: folderName, inJsonFileName: FileName }
+    let LocalItemName = ItemName;
+    let LocalScreenName = ScreenName;
+    let LocalFindColumnObject;
+    let LocalFromUpdate;
+    let LocalReturnObject = { KTF: false };
+    let LocalJsonTableColumnsKey = "TableColumns";
+
+    let LocalFromPullData = await CommonPullDataFromConfig.FromJsonConfig({
+        inJsonConfig,
+        inDataPK: LocalinDataPK
+    });
+
+    let LocalNewData = JSON.parse(JSON.stringify(LocalFromPullData.JsonData));
+
+    if (LocalItemName in LocalNewData) {
+        if (LocalScreenName in LocalNewData[LocalItemName]) {
+            if ("SubTableColumns" in LocalNewData[LocalItemName][LocalScreenName]) {
+                if ("FinGrid" in LocalNewData[LocalItemName][LocalScreenName].SubTableColumns) {
+                    if ("TableColumns" in LocalNewData[LocalItemName][LocalScreenName].SubTableColumns.FinGrid) {
+
+                        LocalFindColumnObject = _.find(LocalNewData[LocalItemName][LocalScreenName].SubTableColumns.FinGrid.TableColumns, { DataAttribute });
+
+                        LocalFindColumnObject.Widths.px = parseInt(LocalDataToUpdate.px);
+
+                        LocalFromUpdate = await CommonFromPushData.StartFunc({
+                            inFolderName: FolderName,
+                            inFileNameWithExtension: FileName,
+                            inDataPK: LocalinDataPK,
+                            inDataToUpdate: LocalNewData,
+                            inOriginalData: LocalFromPullData.JsonData
+                        });
+
+
+                        if (LocalFromUpdate.KTF) {
+                            LocalReturnObject.KTF = true;
+                        };
+
+                        return await LocalReturnObject;
+                    };
+                };
+            };
+        };
+    };
+
+    return await LocalReturnObject;
+};
+
+// UpdateKeys({
+//     DataPK: 16, folderName: "Masters", FileName: "Customers.json", ItemName: "CustomersName", ScreenName: "Create",
+//     DataAttribute: "pk",
+//     BodyAsJson: {
+//         DisplayName: "ppppppppppp"
+//     }
+// }).then(p => {
+//     console.log("pppp : ", p);
+// });
+
+
+module.exports = {
+    Update
+};
