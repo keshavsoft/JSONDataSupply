a31203f388c03dabab4247dcad4b716359bcd51d
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-12 20:02:30 +0530

    SREENIVAS

--- DataSupply/Fs/LoginFolder/Admin/CreateFolders/Basic.js
+++ DataSupply/Fs/LoginFolder/Admin/CreateFolders/Basic.js
@@ -1,5 +1,20 @@
 const fs = require("fs-extra");
 
+let CommonFirmObject = {
+    Firm: {
+        FirmName: "",
+        FromDate: "01-04-2021",
+        ToDate: "31-03-2022",
+        Config: {
+            Ui: {
+                Login: {
+                    "RedirectPage": "/JSONApi/Html/UserData/Menu/AsCards/Files.html"
+                }
+            }
+        }
+    }
+};
+
 let StartFunc = async ({ inFolderPath }) => {
     let LocalReturnData = { KTF: false, KReason: "" };
 
@@ -8,6 +23,8 @@ let StartFunc = async ({ inFolderPath }) => {
         fs.mkdirSync(`${inFolderPath}/Data`, { recursive: true });
         fs.mkdirSync(`${inFolderPath}/Config`, { recursive: true });
         fs.mkdirSync(`${inFolderPath}/Admin`, { recursive: true });
+        fs.writeFileSync(`${inFolderPath}/FirmDetails.json`, JSON.stringify(CommonFirmObject));
+
         
         LocalReturnData.KTF = true;
     } catch (error) {
