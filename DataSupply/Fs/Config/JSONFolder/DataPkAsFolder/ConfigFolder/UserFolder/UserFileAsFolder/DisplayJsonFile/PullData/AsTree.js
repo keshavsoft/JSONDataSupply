1b12898b6c2386374def8c285fda6139d0244129
Keshav <nknnkumar@yahoo.com>
2023-01-18 20:29:02 +0530

    keshav

--- DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/PullData/AsTree.js
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/PullData/AsTree.js
@@ -2,20 +2,84 @@ let _ = require("lodash");
 
 let CommonFromAsJson = require("./AsJson");
 
-// LocalLoopObject = {};
-// LocalLoopObject.ItemName = key;
-// LocalLoopObject.ItemNameForHtmlId = key.replace(" ", "_");
-// //    LocalLoopObject.RowCount = Object.keys(value).length;
-// LocalLoopObject.Screens = CommonScreens.RowsAsObjectsWithColumns({
-//     inFolderName,
-//     inFileNameWithExtension,
-//     inItemName: key,
-//     inUserPK
-// });
-
-// LocalReturnObject[key.replace(" ", "_")] = LocalLoopObject;
-
 let AsObjects = async ({ inFolderName, inFileNameOnly, inDataPK }) => {
+    let LocalDataPK = inDataPK;
+
+    let LocalReturnObject = {
+        KTF: false,
+        JsonData: {}
+    };
+
+    if (LocalDataPK > 0) {
+        let LocalDataFromCommonCreate;
+        let LocalFolderName = inFolderName;
+        let LocalFileNameOnly = inFileNameOnly;
+
+        LocalDataFromCommonCreate = await CommonFromAsJson.StartFunc({
+            inFolderName: LocalFolderName,
+            inFileNameOnly: LocalFileNameOnly,
+            inDataPK: LocalDataPK
+        });
+
+        if (LocalDataFromCommonCreate.KTF === false) {
+            LocalReturnObject.KReason = LocalDataFromCommonCreate.KReason;
+            return await LocalReturnObject;
+        };
+
+        Object.entries(LocalDataFromCommonCreate.JsonData).forEach(
+            ([key, value]) => {
+                LocalLoopObject = {};
+                LocalLoopObject.ItemName = key;
+                LocalLoopObject.ItemNameForHtmlId = key.replace(" ", "_");
+                LocalLoopObject.Screens = {};
+
+                Object.entries(value).forEach(
+                    ([ScreenKey, ScreenValue]) => {
+                        let LocalScreenObject = {};
+                        LocalScreenObject.ScreenName = ScreenKey;
+                        LocalScreenObject.ScreenNameForHtmlId = ScreenKey.replace(" ", "_");
+                        LocalScreenObject.TableInfo = ScreenValue.TableInfo;
+
+                        LocalScreenObject.TableColumnsObject = {};
+
+                        ScreenValue.TableColumns.forEach(LoopTableColumn => {
+                            LocalScreenObject.TableColumnsObject[LoopTableColumn.DataAttribute] = LoopTableColumn;
+                        });
+
+                        if ("SubTableColumns" in ScreenValue) {
+                            LocalScreenObject.SubTableColumnsObject = {};
+
+                            Object.entries(ScreenValue.SubTableColumns).forEach(
+                                ([SubTableColumnskey, SubTableColumnsvalue]) => {
+                                    let LoopInsideObject = {};
+                                    LoopInsideObject.TableColumnsObject = {};
+
+                                    SubTableColumnsvalue.TableColumns.forEach(LoopTableColumn => {
+                                        LoopInsideObject.TableColumnsObject[LoopTableColumn.DataAttribute] = LoopTableColumn;
+                                    });
+
+                                    LocalScreenObject.SubTableColumnsObject[SubTableColumnskey] = LoopInsideObject;
+                                }
+                            );
+                        };
+
+                        // LocalScreenObject.SubTableColumns = ScreenValue.SubTableColumns;
+
+                        LocalLoopObject.Screens[ScreenKey] = LocalScreenObject;
+                    }
+                );
+
+                LocalReturnObject.JsonData[key.replace(" ", "_")] = LocalLoopObject;
+            }
+        );
+
+        LocalReturnObject.KTF = true;
+    };
+
+    return await LocalReturnObject;
+};
+
+let AsObjects_18Jan2022 = async ({ inFolderName, inFileNameOnly, inDataPK }) => {
     let LocalDataPK = inDataPK;
 
     let LocalReturnObject = {
@@ -72,24 +136,6 @@ let AsObjects = async ({ inFolderName, inFileNameOnly, inDataPK }) => {
 
     return await LocalReturnObject;
 };
-
-// AsObjects({
-//     inFolderName: "Masters",
-//     inFileNameOnly: "Accounts",
-//     inDataPK: 1022
-// }).then(p => {
-//     console.log("pppp : ", p.JsonData.Accounts.Screens.Create.TableColumnsObject);
-// });
-
-// FromJsonConfig({
-//     inJsonConfig:{
-//         inFolderName: "Masters",
-//         inJsonFileName: "Customers.json"
-//     },
-//     inDataPK: 16
-// }).then(p => {
-//     console.log("pppp : ", p);
-// });
 
 module.exports = {
     AsObjects
