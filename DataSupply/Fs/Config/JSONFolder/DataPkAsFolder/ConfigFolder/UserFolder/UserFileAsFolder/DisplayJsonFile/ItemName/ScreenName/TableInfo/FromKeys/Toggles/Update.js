c5834ca421ac9eff757fe040625ec633b90f74d0
Keshav <nknnkumar@yahoo.com>
2023-01-18 12:13:08 +0530

    keshav

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/TableInfo/FromKeys/Toggles/Update.js
@@ -0,0 +1,69 @@
+let _ = require("lodash");
+
+//let CommonPullDataFromConfig = require("../../../../../PullData/AsJson");
+let CommonPullDataFromConfig = require("../../../../../PullData/AsJson");
+
+let CommonFromPushData = require("../../../../../PushData/FromFoldFile");
+
+let Update = async ({ DataPK, FolderName, FileName, ItemName, ScreenName, BodyAsJson }) => {
+
+    const LocalDataToUpdate = (({ ShowFooter }) => ({ ShowFooter }))(BodyAsJson);
+
+    let LocalinDataPK = DataPK;
+
+    let LocalItemName = ItemName;
+    let LocalScreenName = ScreenName;
+    let LocalFromUpdate;
+    let LocalReturnObject = { KTF: false };
+
+    let LocalFromPullData = await CommonPullDataFromConfig.FromFoldFile({
+        inFolderName: FolderName,
+        inFileNameWithExtension: `${FileName}.json`,
+        inDataPK: LocalinDataPK
+    });
+
+    let LocalNewData = JSON.parse(JSON.stringify(LocalFromPullData.JsonData));
+
+    if (LocalItemName in LocalNewData) {
+        if (LocalScreenName in LocalNewData[LocalItemName]) {
+            if ("TableInfo" in LocalNewData[LocalItemName][LocalScreenName]) {
+                LocalNewData[LocalItemName][LocalScreenName].TableInfo.ShowFooter = LocalDataToUpdate.ShowFooter;
+
+                // LocalNewData[LocalItemName][LocalScreenName].TableInfo.FooterType.ShowBalance = LocalDataToUpdate.ShowBalance;
+                // LocalNewData[LocalItemName][LocalScreenName].TableInfo.FooterType.CreateNew = LocalDataToUpdate.CreateNew;
+                // LocalNewData[LocalItemName][LocalScreenName].TableInfo.FooterType.ShowTotals = LocalDataToUpdate.ShowTotals;
+
+                LocalFromUpdate = await CommonFromPushData.StartFunc({
+                    inFolderName: FolderName,
+                    inFileNameWithExtension: FileName,
+                    inDataPK: LocalinDataPK,
+                    inDataToUpdate: LocalNewData,
+                    inOriginalData: LocalFromPullData.JsonData
+                });
+
+                if (LocalFromUpdate.KTF) {
+                    LocalReturnObject.KTF = true;
+                };
+
+                return await LocalReturnObject;
+            };
+        };
+    };
+
+    return await LocalReturnObject;
+};
+
+// UpdateKeys({
+//     DataPK: 16, folderName: "Masters", FileName: "Customers.json", ItemName: "CustomersName", ScreenName: "Create",
+//     DataAttribute: "pk",
+//     BodyAsJson: {
+//         DisplayName: "ppppppppppp"
+//     }
+// }).then(p => {
+//     console.log("pppp : ", p);
+// });
+
+
+module.exports = {
+    Update
+};
