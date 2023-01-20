1dca19bc4d57b0a513f46d155151169f0f269d91
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-01-20 12:45:08 +0530

    SREENIVAS

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/ItemName/ScreenName/SubTableColumns/DisplayNameAlter/Update.js
@@ -0,0 +1,72 @@
+let _ = require("lodash");
+
+let CommonPullDataFromConfig = require("../../../../PullData/AsJson");
+let CommonFromPushData = require("../../../../PushData/FromFoldFile");
+
+
+let Update = async ({ DataPK, folderName, FileName, ItemName, ScreenName, subtablecolumnkey, DataAttribute, BodyAsJson }) => {
+    console.log("BodyAsJson", BodyAsJson, DataAttribute, subtablecolumnkey);
+    const LocalDataToUpdate = (({ DisplayName }) => ({ DisplayName }))(BodyAsJson);
+    let LocalinDataPK = DataPK;
+
+    let inJsonConfig = { inFolderName: folderName, inJsonFileName: FileName }
+    let LocalItemName = ItemName;
+    let LocalScreenName = ScreenName;
+    let LocalFindColumnObject;
+    let LocalFromUpdate;
+    let LocalReturnObject = { KTF: false };
+    let LocalJsubtablecolumnkey = subtablecolumnkey;
+
+    let LocalFromPullData = await CommonPullDataFromConfig.FromJsonConfig({
+        inJsonConfig,
+        inDataPK: LocalinDataPK
+    });
+
+    let LocalNewData = JSON.parse(JSON.stringify(LocalFromPullData.JsonData));
+
+    if (LocalItemName in LocalNewData) {
+        if (LocalScreenName in LocalNewData[LocalItemName]) {
+            if ("SubTableColumns" in LocalNewData[LocalItemName][LocalScreenName]) {
+                if (LocalJsubtablecolumnkey in LocalNewData[LocalItemName][LocalScreenName].SubTableColumns) {
+                    if ("TableColumns" in LocalNewData[LocalItemName][LocalScreenName].SubTableColumns[LocalJsubtablecolumnkey]) {
+                        LocalFindColumnObject = _.find(LocalNewData[LocalItemName][LocalScreenName].SubTableColumns[LocalJsubtablecolumnkey].TableColumns, { DataAttribute });
+
+                        LocalFindColumnObject.DisplayName = LocalDataToUpdate.DisplayName;
+
+                        LocalFromUpdate = await CommonFromPushData.StartFunc({
+                            inFolderName: folderName,
+                            inFileNameWithExtension: FileName,
+                            inDataPK: LocalinDataPK,
+                            inDataToUpdate: LocalNewData,
+                            inOriginalData: LocalFromPullData.JsonData
+                        });
+
+
+                        if (LocalFromUpdate.KTF) {
+                            LocalReturnObject.KTF = true;
+                        };
+
+                        return await LocalReturnObject;
+                    };
+                };
+            };
+        };
+    };
+
+    return await LocalReturnObject;
+};
+
+// UpdateKeys({
+//     DataPK: 16, folderName: "Masters", FileName: "Customers.json", ItemName: "CustomersName", ScreenName: "Create",
+//     DataAttribute: "pk",
+//     BodyAsJson: {
+//         DisplayName: "ppppppppppp"
+//     }
+// }).then(p => {
+//     console.log("pppp : ", p);
+// });
+
+
+module.exports = {
+    Update
+};
