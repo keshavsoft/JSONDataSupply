be5daad9f2fb937a5b5bf5ffcec10bc01cc1af6d
JatinNeeraj <113779133+JatinNeeraj@users.noreply.github.com>
2023-02-04 15:12:20 +0530

    jatin

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ReportsFolder/LedgerAutoJsonFile/InsideReport/FromKeys/ColumnNameToPick/Update.js
@@ -0,0 +1,69 @@
+let _ = require("lodash");
+
+let CommonPullDataFromConfig = require("../../../PullDataFromFile/FromJson");
+let CommonFromPushData = require("../../../PushDataFromFile/FromJson");
+
+let Update = async ({ DataPK, ItemName, voucher, BodyAsJson }) => {
+    const LocalDataToUpdate = (({ FolderName, FileName, Active }) => ({ FolderName, FileName, Active }))(BodyAsJson);
+    let LocalinDataPK = DataPK;
+    let LocalReportName = ItemName;
+    let LocalVouchersConsiderPk = parseInt(voucher);
+
+    let LocalFromUpdate;
+    let LocalReturnObject = { KTF: false };
+
+    let LocalFromPullData = await CommonPullDataFromConfig.StartFunc({
+        inDataPK: LocalinDataPK
+    });
+
+    if (LocalFromPullData.KTF === false) {
+        LocalReturnObject.KReason = LocalFromPullData.KReason;
+        return LocalReturnObject;
+    };
+    let LocalNewData = JSON.parse(JSON.stringify(LocalFromPullData.JsonData));
+
+    if (LocalReportName in LocalNewData) {
+        if ("VouchersConsider" in LocalNewData[LocalReportName]) {
+            let LocalFilterObject = {};
+            LocalFilterObject.pk = LocalVouchersConsiderPk;
+            LocalFindColumnObject = _.find(LocalNewData[LocalReportName].VouchersConsider, LocalFilterObject);
+
+            console.log("ooooooo---", LocalFindColumnObject.FolderName, LocalDataToUpdate.FolderName);
+
+            LocalFindColumnObject.FolderName = LocalDataToUpdate.FolderName;
+            LocalFindColumnObject.FileName = LocalDataToUpdate.FileName;
+            LocalFindColumnObject.Active = LocalDataToUpdate.Active;
+
+
+            LocalFromUpdate = await CommonFromPushData.StartFunc({
+                inDataPK: LocalinDataPK,
+                inDataToUpdate: LocalNewData,
+                inOriginalData: LocalFromPullData.JsonData
+            });
+
+            if (LocalFromUpdate.KTF) {
+                LocalReturnObject.KTF = true;
+            };
+
+            return await LocalReturnObject;
+
+        };
+    };
+
+
+    return await LocalReturnObject;
+};
+let MockFunc = () => {
+    Update({
+        DataPK: 1024,
+        ItemName: "StockBalances",
+        voucher: "20"
+    }).then((PromiseData) => {
+        console.log("PromiseData--", Object.keys(PromiseData));
+    })
+};
+// MockFunc();
+
+module.exports = {
+    Update
+};
