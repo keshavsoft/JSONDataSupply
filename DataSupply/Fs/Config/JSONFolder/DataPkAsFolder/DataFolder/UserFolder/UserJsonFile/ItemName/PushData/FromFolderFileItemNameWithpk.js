d38a116c48a46c3f237d9a3b5116981906c12714
keshavsoft <nknnkumar@yahoo.com>
2023-03-28 17:30:01 +0530

    keshav

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/FromFolderFileItemNameWithpk.js
@@ -0,0 +1,50 @@
+let CommonFromPullDataFromFile = require("../../PullDataFromFile/FromFolderAndFile");
+let CommonFromPushDataToFile = require("../../PushDataToFile/FolderAndFile");
+
+let StartFunc = async ({ inFolderName, inFileNameOnly, inItemName, inDataPK, inDataToInsert, inpk }) => {
+    let LocalinFolderName = inFolderName;
+    let LocalinFileNameOnly = inFileNameOnly;
+    let LocalinItemName = inItemName;
+
+    let LocalinDataPK = inDataPK;
+    let LocalReturnData = { KTF: false, DirPath: "", CreatedLog: {} };
+
+    let LocalFromCommonFromCheck = CommonFromPullDataFromFile.StartFunc({
+        inFolderName: LocalinFolderName,
+        inFileNameOnly: LocalinFileNameOnly,
+        inDataPK: LocalinDataPK
+    });
+
+    if (LocalFromCommonFromCheck.KTF === false) {
+        LocalReturnData.KReason = LocalFromCommonFromCheck.KReason;
+        return LocalReturnData;
+    };
+
+    if ((LocalinItemName in LocalFromCommonFromCheck.JsonData) === false) {
+        LocalReturnData.KReason = `Item Name : ${LocalinItemName} not found!`;
+        return LocalReturnData;
+    };
+
+    LocalFromCommonFromCheck.JsonData[LocalinItemName][inpk] = inDataToInsert;
+
+    let LocalFromPush = await CommonFromPushDataToFile.InsertToJson({
+        inFolderName: LocalinFolderName,
+        inFileNameOnly: LocalinFileNameOnly,
+        inDataPK: LocalinDataPK,
+        inDataToUpdate: LocalFromCommonFromCheck.JsonData,
+        inOriginalData: ""
+    });
+
+    LocalReturnData.KTF = true;
+
+    return await LocalReturnData;
+};
+
+// console.log("ForExistence----- : ", ReturnAsArrayWithPKSortByPK({
+//     inFolderName: "Transactions",
+//     inFileNameOnly: "GST-SALES",
+//     inItemName: "FERTLIZERS-GST--SALES",
+//     inDataPK: 1024
+// }).JsonData[0]);
+
+module.exports = { StartFunc };
---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/InsertWithPk.js
@@ -0,0 +1,66 @@
+let CommonFromPushData = require("./FromFolderFileItemNameWithpk");
+let CommonFromPullData = require("../PullData/FromFolderFileItemName");
+
+let StartFunc = async ({ inFolderName, inFileNameOnly, inItemName, inDataPK, inDataToInsert }) => {
+
+    const LocalDataObject = (({ pk }) => ({ pk }))(inDataToInsert)
+
+    let LocalinFolderName = inFolderName;
+    let LocalinFileNameOnly = inFileNameOnly;
+    let LocalinItemName = inItemName;
+
+    let localpk = LocalDataObject.pk
+
+    let LocalinDataPK = inDataPK;
+    let LocalReturnData = { KTF: false, DirPath: "", CreatedLog: {} };
+
+    let LocalFromCommonFromPullData = CommonFromPullData.StartFunc({
+        inFolderName: LocalinFolderName,
+        inFileNameOnly: LocalinFileNameOnly,
+        inItemName: LocalinItemName,
+        inDataPK: LocalinDataPK
+    });
+
+    if (LocalFromCommonFromPullData.KTF === false) {
+        LocalReturnData.KReason = LocalFromCommonFromPullData.KReason;
+        return await LocalReturnData;
+    };
+
+    let LocalNewData = JSON.parse(JSON.stringify(LocalFromCommonFromPullData.JsonData));
+
+    if (localpk in LocalNewData) {
+        LocalReturnData.KReason = `${localpk} Already Found !`;
+
+        return await LocalReturnData;
+    };
+
+    if ((localpk in LocalNewData) === false) {
+
+        let LocalFromCommonFromPushDataToFile = await CommonFromPushData.StartFunc({
+            inFolderName: LocalinFolderName,
+            inFileNameOnly: LocalinFileNameOnly,
+            inItemName: LocalinItemName,
+            inpk: localpk,
+            inDataToInsert: inDataToInsert,
+            inDataPK: LocalinDataPK
+        });
+        
+        if (LocalFromCommonFromPushDataToFile.KTF === false) {
+            LocalReturnData.KReason = LocalFromCommonFromPushDataToFile.KReason;
+            return await LocalReturnData;
+        };
+
+        LocalReturnData.KTF = true;
+    };
+
+    return await LocalReturnData;
+};
+
+// console.log("ForExistence----- : ", ReturnAsArrayWithPKSortByPK({
+//     inFolderName: "Transactions",
+//     inFileNameOnly: "GST-SALES",
+//     inItemName: "FERTLIZERS-GST--SALES",
+//     inDataPK: 1024
+// }).JsonData[0]);
+
+module.exports = { StartFunc };
