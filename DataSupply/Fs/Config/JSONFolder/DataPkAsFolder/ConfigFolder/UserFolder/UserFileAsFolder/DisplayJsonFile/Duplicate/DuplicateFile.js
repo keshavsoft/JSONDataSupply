4945d3b7286aec65365b2a145d706d19e46d295f
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-04-15 17:52:42 +0530

    SREENIVAS

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/UserFolder/UserFileAsFolder/DisplayJsonFile/Duplicate/DuplicateFile.js
@@ -0,0 +1,51 @@
+let localpullFolders = require("../../../getDirectories");
+let fs = require("fs-extra");
+let srcDir;
+let distDir;
+
+let StartFunc = async ({ inFolderName, inFileNameOnly, inToFileName, inDataPK }) => {
+
+    let LocalReturnData = { KTF: false, KReason: "" };
+
+
+    let localFilesAsArray = await localpullFolders.AsObjects({ inFolderName, inDataPK });
+    console.log(localFilesAsArray.FolderPath);
+    if (localFilesAsArray.KTF) {
+        LocalReturnData.KReason = localFilesAsArray.KReason;
+        LocalReturnData.FolderPath = localFilesAsArray.FolderPath;
+
+        return await LocalReturnData;
+    };
+
+    if (inFileNameOnly in localFilesAsArray === false) {
+        LocalReturnData.KReason = `inFileName: ${inFileNameOnly} not found in config.json!`
+        return await LocalReturnData;
+    };
+
+    if (inToFileName in localFilesAsArray) {
+        LocalReturnData.KReason = `FileName: ${inToFileName} found in Folder.!`
+        return await LocalReturnData;
+    };
+
+
+    fs.copy('/path/to/source', '/path/to/destination', function (err) {
+        if (err) return console.error(err)
+        console.log('success!')
+    });
+
+
+
+};
+let localMockFunc = async () => {
+    let localdata = await StartFunc({
+        inFolderName: "Transactions",
+        inFileNameOnly: "GST-SALES-",
+        inToFileName: "GST-PURCHASE",
+        inDataPK: "1022"
+
+    });
+    console.log("localdata", localdata);
+};
+localMockFunc();
+
+module.exports = { StartFunc };
