73aab9a1d1bf19333a2e67b1c8b71c82c046090d
Keshav <nknnkumar@yahoo.com>
2023-05-06 19:59:25 +0530

    keshav

---
+++ DataSupply/Fs/Config/JSONUploadFolder/UserFolders/PullFolders/getDirectories.js
@@ -0,0 +1,32 @@
+const fs = require("fs");
+let CommonFromCheck = require("../../Check");
+
+let StartFunc = () => {
+    let LocalFromCommonFromCheck = CommonFromCheck.ForExistence();
+
+    if (LocalFromCommonFromCheck.KTF === false) {
+        return [];
+    };
+
+    let LocalDataPath = `${LocalFromCommonFromCheck.JSONFolderPath}`;
+  
+    let LocalReturnArray = fs.readdirSync(LocalDataPath).filter(function (file) {
+        return fs.statSync(LocalDataPath + '/' + file).isDirectory();
+    });
+
+    let LocalNumberArray = LocalReturnArray.map(element => parseInt(element));
+
+    let LocalFilterArray = LocalNumberArray.filter(element => {
+        return isNaN(element) === false;
+    });
+
+    return LocalFilterArray;
+};
+
+let LocalMockFunc = () => {
+    let LocalData = StartFunc();
+};
+
+// LocalMockFunc();
+
+module.exports = { StartFunc };
