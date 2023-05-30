b2b69c8c3a827778da8924f28f9bdd1a793f80c5
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-05-30 22:07:44 +0530

    keshav

--- DataSupply/Fs/Config/Compare/Upload-Json/ForPk.js
+++ DataSupply/Fs/Config/Compare/Upload-Json/ForPk.js
@@ -1,5 +1,7 @@
 let CommonJSONUploadFolder = require("../../JSONUploadFolder/UserFolders/PullFolders/getDirectories");
 let CommonJSONFolder = require("../../JSONFolder/UserFolders/PullFolders/getDirectories");
+let CommonLoginFolder = require("../../JSONFolder/LoginFolder/UserDataJsonFile/PullDataFromFile/FromJson");
+let CommonMockAllow = require("../../../../MockAllow.json");
 
 let StartFunc = () => {
     let LocaFromCommonJSONUploadFolder = CommonJSONUploadFolder.StartFunc();
@@ -8,6 +10,55 @@ let StartFunc = () => {
     LocalReturnData.FolderData = {};
     LocalReturnData.KTF = true;
 
+    let LocalFromCommonLoginFolder = CommonLoginFolder.StartFunc();
+
+    LocalReturnData = { ...LocalFromCommonLoginFolder };
+
+    if (LocalFromCommonLoginFolder.KTF === false) {
+        return LocalReturnData;
+    };
+
+    LocalReturnData.FolderData = LocaFromCommonJSONUploadFolder.map(element => {
+        let LoopInsideObject = {};
+        LoopInsideObject[element] = {};
+
+        LoopInsideObject[element].JsonPk = element;
+        LoopInsideObject[element].DataMissing = false;
+
+        let LocalFilteredArray = LocaFromCommonJSONUploadFolder.filter(element => LocaCommonJSONFolder.includes(element));
+
+        if (LocalFilteredArray.length === 0) {
+            LoopInsideObject[element].DataMissing = true;
+        };
+
+        if (element in LocalFromCommonLoginFolder.JsonData) {
+            LoopInsideObject[element].UserName = LocalFromCommonLoginFolder.JsonData[element].UserName;
+            LoopInsideObject[element].PassWord = LocalFromCommonLoginFolder.JsonData[element].PassWord;
+        };
+
+        return LoopInsideObject;
+    });
+
+    return LocalReturnData;
+};
+
+
+let StartFunc_30May = () => {
+    let LocaFromCommonJSONUploadFolder = CommonJSONUploadFolder.StartFunc();
+    let LocaCommonJSONFolder = CommonJSONFolder.StartFunc();
+    let LocalReturnData = {};
+    LocalReturnData.FolderData = {};
+    LocalReturnData.KTF = true;
+
+    let LocalFromCommonLoginFolder = CommonLoginFolder.StartFunc();
+
+    LocalReturnData = { ...LocalFromCommonLoginFolder };
+
+    if (LocalFromCommonLoginFolder.KTF === false) {
+        return LocalReturnData;
+    };
+
+    console.log("LocalFromCommonLoginFolder : ", LocalFromCommonLoginFolder);
     LocaFromCommonJSONUploadFolder.forEach(element => {
         LocalReturnData.FolderData[element] = {};
         LocalReturnData.FolderData[element].JsonPk = element;
@@ -26,6 +77,13 @@ let StartFunc = () => {
 let LocalMockFunc = () => {
     let LocalData = StartFunc();
     console.log("LocalData", LocalData);
+};
+
+if (CommonMockAllow.AllowMock) {
+    if (CommonMockAllow.MockKey === "Keshav30") {
+        let LocalData = StartFunc();
+        console.log("LocalData", LocalData, LocalData.FolderData);
+    };
 };
 
 // LocalMockFunc();
