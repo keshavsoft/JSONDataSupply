5f2d53a2624fb6804d98b446e018a35f3cd63771
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-06-13 15:52:49 +0530

    keshav

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/AdminFolder/PreloadJsonFile/FromKey/Delete/GetFunc.js
@@ -0,0 +1,31 @@
+let CommonPullDataFromFile = require("../../PullDataFromFile/FromJson");
+
+let MockAllowFunc = require("../../../../../../../../MockAllow.json")
+
+let StartFunc = ({ DataPK }) => {
+    let LocalinDataPK = DataPK;
+    let LocalReturnData = {};
+
+    let LocalFromCommonCheckKey = CommonPullDataFromFile.StartFunc({
+        DataPK: LocalinDataPK
+    });
+
+    LocalReturnData = { ...LocalFromCommonCheckKey };
+
+    if (LocalFromCommonCheckKey.KTF === false) {
+        return LocalReturnData;
+    };
+
+    return LocalReturnData;
+};
+
+if (MockAllowFunc.AllowMock) {
+    if (MockAllowFunc.MockKey === "K13") {
+        let result = StartFunc({
+            DataPK: MockAllowFunc.DataPK
+        });
+        console.log("result : ", result);
+    };
+};
+
+module.exports = { StartFunc };
