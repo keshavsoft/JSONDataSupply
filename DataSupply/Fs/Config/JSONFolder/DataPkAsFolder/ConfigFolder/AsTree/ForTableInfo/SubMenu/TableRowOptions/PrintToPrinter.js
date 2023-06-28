b41739d9e7cb13c6cbc3b90777f6c44a48d66b16
JatinNeeraj <jatinucm123@gmail.com>
2023-06-28 18:29:28 +0530

    neeraj55

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/ConfigFolder/AsTree/ForTableInfo/SubMenu/TableRowOptions/PrintToPrinter.js
@@ -0,0 +1,48 @@
+let CommonFromgetDirectories = require("../../../../getDirectoriesWithCheckAndDelete");
+let CommonMockAllow = require("../../../../../../../../../MockAllow.json");
+
+let AsObject = async ({ inDataPK }) => {
+    let LocalDataPK = inDataPK;
+    let LocalKeyNeeded = "PrintToPrinter";
+
+    let LocalFromCommon = await CommonFromgetDirectories.AsObject({ inDataPK: LocalDataPK });
+
+    Object.entries(LocalFromCommon.Folders).forEach(
+        ([KeyForFolder, ValueForFolder]) => {
+            Object.entries(ValueForFolder.Files).forEach(
+                ([KeyForFiles, ValueForFiles]) => {
+                    Object.entries(ValueForFiles.Items).forEach(
+                        ([KeyForItems, ValueForItems]) => {
+                            Object.entries(ValueForItems.Screens).forEach(
+                                ([KeyForScreens, ValueForScreens]) => {
+                                    delete ValueForScreens.SubTableInfo;
+                                    delete ValueForScreens.TableColumnsObject;
+                                    delete ValueForScreens.ReturnDataJsonContent;
+                                    let LoopInsideTableRowOptions = ValueForScreens.TableInfo.TableRowOptions[LocalKeyNeeded];
+
+                                    ValueForScreens.TableInfo = {};
+                                    ValueForScreens.TableInfo.TableRowOptions = {};
+                                    ValueForScreens.TableInfo.TableRowOptions[LocalKeyNeeded] = LoopInsideTableRowOptions;
+                                }
+                            );
+                        }
+                    );
+                }
+            );
+        }
+    );
+
+    return await LocalFromCommon;
+};
+
+if (CommonMockAllow.AllowMock) {
+    if (CommonMockAllow.MockKey === "TABLE") {
+        AsObject({
+            inDataPK: CommonMockAllow.DataPK
+        }).then(FromPromise => {
+            console.log("FromPromise : ", FromPromise.Folders.Trans.Files.PAYMENTS.Items["BANK-CASH-DEPOSITS"].Screens.Show.TableInfo.TableRowOptions);
+        });
+    };
+};
+
+module.exports = { AsObject };
