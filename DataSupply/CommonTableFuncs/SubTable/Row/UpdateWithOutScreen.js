d3d9767fbd7f6d3593313261a1aff669da73117c
nknnkumar <115708070+nknnkumar@users.noreply.github.com>
2023-08-09 20:41:48 +0530

    keshav

--- DataSupply/CommonTableFuncs/SubTable/Row/UpdateWithOutScreen.js
+++ DataSupply/CommonTableFuncs/SubTable/Row/UpdateWithOutScreen.js
@@ -1,12 +1,8 @@
-let CommonCheckBeforeSave = require("../../Vertical/CheckBeforeSave");
-let CommonDefaultValue = require("../../ToUi/CalculateDefaultValue");
+let CommonDisplayPullData = require("../../../Fs/Config/Folders/Files/ConfigFromDisplayJson/Screens/Items/PullData/FromDisplayJson/FromJson");
+let CommonFilesPullData = require("../../../Fs/Config/Folders/Files/PullData/FromData");
+let CommonFilesPushData = require("../../../Fs/Config/Folders/Files/PushData/ToData");
 
-let CommonDisplayPullData = require("../../Fs/Config/Folders/Files/ConfigFromDisplayJson/Screens/Items/PullData/FromDisplayJson/FromJson");
-let CommonFilesPullData = require("../../Fs/Config/Folders/Files/PullData/FromData");
-let CommonFilesPushData = require("../../Fs/Config/Folders/Files/PushData/ToData");
-let CommonSaveFuncs = require("../../SaveFuncs");
-
-let CommonMock = require("../../MockAllow.json");
+let CommonMock = require("../../../MockAllow.json");
 
 let LocalUpdateRow = ({ inOriginalData, inPostData }) => {
     Object.entries(inPostData).forEach(
@@ -29,10 +25,7 @@ let WithTransformBeforeSave = async ({ JsonConfig, ItemConfig, UserPK, inDataToU
     let LocalUserData;
     let LocalUserDataWithItemName;
     let LocalConfigData;
-    let LocalConfigDataColumns;
-    let LocalFromServerSideCheck;
     let LocalUpdatedData;
-    let LocalSubTableColumnsKey = "SubTableColumns";
 
     if ((inUserPK > 0) === false) {
         LocalReturnObject.KReason = `${inUserPK} not found in Data as folder!`;
@@ -43,37 +36,13 @@ let WithTransformBeforeSave = async ({ JsonConfig, ItemConfig, UserPK, inDataToU
     LocalConfigData = await CommonDisplayPullData.AsReturnObject({ inJsonConfig, inItemConfig, inDataPK: inUserPK });
 
     if (LocalConfigData.KTF) {
-        if ((LocalSubTableColumnsKey in LocalConfigData.JsonData) === false) {
-            LocalReturnObject.KReason = `SubTableColumns not found in Config`;
-            return await LocalReturnObject;
-        };
-
-        if ((inSubTableKey in LocalConfigData.JsonData[LocalSubTableColumnsKey]) === false) {
-            LocalReturnObject.KReason = `inSubTableKey : ${inSubTableKey} in not found in Config`;
-            return await LocalReturnObject;
-        };
-
-        LocalConfigDataColumns = LocalConfigData.JsonData[LocalSubTableColumnsKey][inSubTableKey].TableColumns;
-
         LocalUpdatedData = JSON.parse(JSON.stringify(LocalUserData));
         LocalUserDataWithItemName = LocalUpdatedData[inItemConfig.inItemName];
 
-        CommonDefaultValue.CalculateDefaultValue({
-            inColumns: LocalConfigDataColumns,
-            inData: LocalUserDataWithItemName, inPostData
-        });
-
-        inPostData = CommonSaveFuncs.LocalTransformObjectBeforeSaving({
-            inDisplayColumns: LocalConfigDataColumns,
-            inObjectToInsert: inPostData
-        });
-
-        LocalFromServerSideCheck = CommonCheckBeforeSave.ServerSideCheck({
-            inItemConfig,
-            inUserData: LocalUpdatedData,
-            inConfigData: LocalConfigData.JsonData,
-            inObjectToInsert: inPostData, inUserPK
-        });
+        if ((inRowPK in LocalUserDataWithItemName) === false) {
+            LocalReturnObject.KReason = `SubTableColumns not found in Config`;
+            return await LocalReturnObject;
+        };
 
         if ((inSubTableKey in LocalUserDataWithItemName[inRowPK]) === false) {
             LocalReturnObject.KReason = `inSubTableKey : ${inSubTableKey} in not found in PK : ${inRowPK}`;
@@ -83,13 +52,13 @@ let WithTransformBeforeSave = async ({ JsonConfig, ItemConfig, UserPK, inDataToU
         if ((inSubTableRowPK in LocalUserDataWithItemName[inRowPK][inSubTableKey]) === false) {
             LocalReturnObject.KReason = `inSubTableRowPK : ${inSubTableRowPK} in not found in PK : ${inSubTableKey}`;
             return await LocalReturnObject;
-        }
-        console.log("start : ", inPostData, LocalUserDataWithItemName[inRowPK][inSubTableKey][inSubTableRowPK]);
+        };
+
         LocalUpdateRow({
             inOriginalData: LocalUserDataWithItemName[inRowPK][inSubTableKey][inSubTableRowPK],
             inPostData: inPostData
         });
-        console.log("end : ", LocalUserDataWithItemName[inRowPK][inSubTableKey][inSubTableRowPK]);
+
         let PromiseData = await CommonFilesPushData.AsAsync({
             inJsonConfig,
             inUserPK, inOriginalData: LocalUserData,
@@ -106,7 +75,7 @@ let WithTransformBeforeSave = async ({ JsonConfig, ItemConfig, UserPK, inDataToU
 
 if (CommonMock.AllowMock) {
     if (CommonMock.MockKey === 'KES') {
-        let LocalMockData = require('./Update.json');
+        let LocalMockData = require('./UpdateWithOutScreen.json');
 
         WithTransformBeforeSave({
             UserPK: CommonMock.DataPK,
