0e682a8c1534a3271e2b27a1525e2b4309fabfc5
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-08-22 13:59:08 +0530

    SREENIVAS

--- DataSupply/Fs/LoginFolder/Admin/Setup/Garments.js
+++ DataSupply/Fs/LoginFolder/Admin/Setup/Garments.js
@@ -1,26 +1,21 @@
 const fs = require("fs-extra");
 let CommonAbsolutePath = require("../../../DataPath");
-let CommonCreateFolders = require("../CreateFolders/Basic");
+
+let CommonMock = require("../../../../MockAllow.json");
 
 let StartFunc = async ({ inUserPK }) => {
     let LocalReturnData = { KTF: false, KReason: "" };
     try {
-        let LocalReturnFromCreateFolder;
-
         let GlobalDataPath = CommonAbsolutePath.ReturnAbsolutePathOfPresentApp({});
         let LocalFolderPath = `${GlobalDataPath}/${inUserPK}`
-        let LocalFromPath = `${GlobalDataPath}/TemplateDatas/ForTally`;
+        let LocalFromPath = `${GlobalDataPath}/TemplateDatas/ForGarments/902`;
 
         if (fs.existsSync(LocalFolderPath)) {
             LocalReturnData.KReason = "Data is already present on the server";
         } else {
-            LocalReturnFromCreateFolder = await CommonCreateFolders.StartFunc({ inFolderPath: LocalFolderPath });
-            
-            if (LocalReturnFromCreateFolder.KTF) {
-                fs.copySync(LocalFromPath, LocalFolderPath);
+            fs.copySync(LocalFromPath, LocalFolderPath);
 
-                LocalReturnData.KTF = true;
-            };
+            LocalReturnData.KTF = true;
         };
     } catch (error) {
         console.log("error : ", error);
@@ -28,5 +23,17 @@ let StartFunc = async ({ inUserPK }) => {
 
     return await LocalReturnData;
 };
+if (CommonMock.AllowMock) {
+    if (CommonMock.MockKey === 'SSV') {
+        let LocalMockData = require('./Garments.json');
 
+        StartFunc({
+            inDataPK: CommonMock.DataPK,
+            ...LocalMockData
+        }).then(PromiseData => {
+            console.log('PromiseData : ', PromiseData);
+
+        });
+    };
+};
 module.exports = { StartFunc };
