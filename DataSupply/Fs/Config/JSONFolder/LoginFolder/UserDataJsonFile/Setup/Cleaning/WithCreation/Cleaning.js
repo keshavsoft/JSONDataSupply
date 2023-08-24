cd8ce06f4d3fd70fb50d4474e959e8e95c1363a6
keshavsoft <nknnkumar@yahoo.com>
2023-08-24 16:42:05 +0530

    keshav

---
+++ DataSupply/Fs/Config/JSONFolder/LoginFolder/UserDataJsonFile/Setup/Cleaning/WithCreation/Cleaning.js
@@ -0,0 +1,54 @@
+const fs = require("fs-extra");
+// let CommonAbsolutePath = require("../../../DataPath");
+
+let CommonMock = require("../../../../../../../../MockAllow.json");
+let CommonInsert = require("../../../Insert/UserNamePassword");
+
+let StartFunc = ({ inUserName, inPassword }) => {
+    let LocalPullData = CommonInsert.StartFunc({ inUserName, inPassword });
+
+    let LocalReturnObject = { ...LocalPullData };
+    LocalReturnObject.KTF = false;
+
+    if (LocalPullData.KTF === false) {
+        return LocalReturnObject;
+    };
+
+    return LocalReturnObject;
+};
+
+// let StartFunc1 = async ({ inUserPK }) => {
+//     let LocalReturnData = { KTF: false, KReason: "" };
+//     try {
+//         let GlobalDataPath = CommonAbsolutePath.ReturnAbsolutePathOfPresentApp({});
+//         let LocalFolderPath = `${GlobalDataPath}/${inUserPK}`
+//         let LocalFromPath = `${GlobalDataPath}/TemplateDatas/ForLaundry/3016`;
+
+//         if (fs.existsSync(LocalFolderPath)) {
+//             LocalReturnData.KReason = "Data is already present on the server";
+//         } else {
+//             fs.copySync(LocalFromPath, LocalFolderPath);
+
+//             LocalReturnData.KTF = true;
+//         };
+//     } catch (error) {
+//         console.log("error : ", error);
+//     };
+
+//     return await LocalReturnData;
+// };
+
+if (CommonMock.AllowMock) {
+    if (CommonMock.MockKey === 'K5') {
+        let LocalMockData = require('./Cleaning.json');
+
+        let LocalData = StartFunc({
+            inDataPK: CommonMock.DataPK,
+            ...LocalMockData
+        });
+        console.log('LocalData : ', LocalData);
+
+    };
+};
+
+module.exports = { StartFunc };
