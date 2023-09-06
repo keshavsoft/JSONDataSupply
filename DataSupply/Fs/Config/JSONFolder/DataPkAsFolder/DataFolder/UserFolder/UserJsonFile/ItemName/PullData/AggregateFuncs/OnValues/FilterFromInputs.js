1ba3b91b16b415572660e45e56dcb36422f266d1
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-06 18:24:14 +0530

    SREENIVAS

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PullData/AggregateFuncs/OnValues/FilterFromInputs.js
@@ -0,0 +1,52 @@
+// let CommonFromCheck = require("../../../Check");
+let CommonFromFolderFileItemName = require("../../FromFolderFileItemName");
+let _ = require("lodash");
+let CommonMock = require("../../../../../../../../../../../MockAllow.json");
+
+let StartFunc = ({ FolderName, FileNameOnly, ItemName, DataPk, FilterString }) => {
+    let LocalinFolderName = FolderName;
+    let LocalinFileNameOnly = FileNameOnly;
+    let LocalinItemName = ItemName;
+    let localinFilterString = FilterString;
+
+    let LocalinDataPK = DataPk;
+    let LocalReturnData = { KTF: false, DirPath: "", CreatedLog: {} };
+
+    let LocalFromCommonFromCheck = CommonFromFolderFileItemName.StartFunc({
+        inFolderName: LocalinFolderName,
+        inFileNameOnly: LocalinFileNameOnly,
+        inItemName: LocalinItemName,
+        inDataPK: LocalinDataPK
+    });
+
+    if (LocalFromCommonFromCheck.KTF === false) {
+        return LocalReturnData;
+    };
+    // value.CustomerData.CustomerName === "Keshav"
+    // LocalReturnData.JsonData = Object.keys(Object.fromEntries(Object.entries(LocalFromCommonFromCheck.JsonData).filter(([key, value]) =>{
+    //     console.log(eval(localinFilterString));
+
+    // } ))).length
+
+    LocalReturnData.JsonData = Object.keys(Object.fromEntries(Object.entries(LocalFromCommonFromCheck.JsonData).filter(([key, value]) => eval(localinFilterString)))).length
+
+
+    // LocalReturnData.JsonData[LocalMaxPk] = LocalFromCommonFromCheck.JsonData[LocalMaxPk]
+    LocalReturnData.KTF = true;
+
+    return LocalReturnData;
+};
+
+if (CommonMock.AllowMock) {
+    if (CommonMock.MockKey === 'SREE5') {
+        let LocalMockData = require('./FilterFromInputs.json');
+
+        let LocalData = StartFunc({
+            inDataPK: CommonMock.DataPK,
+            ...LocalMockData
+        });
+        console.log("LocalData:", LocalData);
+    };
+};
+
+module.exports = { StartFunc };
