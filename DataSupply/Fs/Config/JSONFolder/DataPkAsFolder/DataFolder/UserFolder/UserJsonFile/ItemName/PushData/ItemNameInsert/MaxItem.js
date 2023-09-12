330c1dcbab8a7352050630c3abe482ed601e711b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-09-12 20:00:44 +0530

    SREENIVAS

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/ItemNameInsert/MaxItem.js
@@ -0,0 +1,51 @@
+let CommonFromFolderAndFile = require("../../../PullDataFromFile/FromFolderAndFile");
+let CommonMock = require("../../../../../../../../../../MockAllow.json");
+
+let StartFunc = ({ inFolderName, inFileNameOnly, inDataPK }) => {
+    let LocalinFolderName = inFolderName;
+    let LocalinFileNameOnly = inFileNameOnly;
+
+    let LocalinDataPK = inDataPK;
+    let LocalReturnData = { KTF: false, DirPath: "", CreatedLog: {} };
+
+    let LocalFromCommonFromCheck = CommonFromFolderAndFile.StartFunc({
+        inFolderName: LocalinFolderName,
+        inFileNameOnly: LocalinFileNameOnly,
+        inDataPK: LocalinDataPK
+    });
+
+    LocalReturnData = { ...LocalFromCommonFromCheck };
+    LocalReturnData.KTF = false;
+
+    if (LocalFromCommonFromCheck.KTF === false) {
+        LocalReturnData.KTFFromRoot = false;
+
+        return LocalReturnData;
+    };
+    let localMaxNumber = Math.max(...(Object.keys(LocalReturnData.JsonData).map(Number)), 0)
+    LocalReturnData.Max = localMaxNumber + 1;
+    LocalReturnData.KTF = true;
+
+
+    return LocalReturnData;
+};
+if (CommonMock.AllowMock) {
+    if (CommonMock.MockKey === '') {
+        let LocalMockData = require('./MaxItem.json');
+
+        let LocalData = StartFunc({
+            inDataPK: CommonMock.DataPK,
+            ...LocalMockData
+        });
+        console.log('LocalData : ', LocalData);
+
+    };
+};
+
+
+
+
+
+module.exports = {
+    StartFunc
+};
