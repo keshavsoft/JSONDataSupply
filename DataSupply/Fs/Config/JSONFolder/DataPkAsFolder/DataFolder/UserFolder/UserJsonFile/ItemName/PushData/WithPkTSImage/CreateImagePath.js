68ea13f04b7245819ab330e0858ee8747a75931b
Srivasu215 <106431000+Srivasu215@users.noreply.github.com>
2023-11-23 13:52:39 +0530

    SREENIVAS

---
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/UserFolder/UserJsonFile/ItemName/PushData/WithPkTSImage/CreateImagePath.js
@@ -0,0 +1,63 @@
+let CommonCreate = require("./Create");
+let CommonMock = require("../../../../../../../../../../MockAllow.json");
+let fs = require("fs");
+
+let StartFunc = ({ inFolderName, inFileNameOnly, inItemName, inDataToInsert, inDataPK }) => {
+
+    let LocalinFolderName = inFolderName;
+    let LocalinFileNameOnly = inFileNameOnly;
+    let LocalinItemName = inItemName;
+    let localinDataToInsert = inDataToInsert;
+    let LocalinDataPK = inDataPK;
+    let LocalImagesPath = "Images";
+
+    let LocalReturnData = { KTF: false, DirPath: "", CreatedLog: {} };
+
+    let LocalFromCommonCommonCreate = CommonCreate.StartFunc({
+        inFolderName: LocalinFolderName,
+        inFileNameOnly: LocalinFileNameOnly,
+        inItemName: LocalinItemName,
+        inDataToInsert: localinDataToInsert,
+        inDataPK: LocalinDataPK
+    });
+
+
+    if (LocalFromCommonCommonCreate.KTF === false) {
+        return LocalReturnData;
+    };
+
+    let LocalRowPk = LocalFromCommonCommonCreate.NewRowPK;
+
+    LocalReturnData.FolderPath = LocalFromCommonCommonCreate.FolderPath;
+    LocalReturnData.UserJsonFileAsFolderPath = `${LocalReturnData.FolderPath}/${LocalinFileNameOnly}`;
+    LocalReturnData.ItemNameAsFolderPath = `${LocalReturnData.UserJsonFileAsFolderPath}/${LocalinItemName}`;
+    LocalReturnData.ImagesFolderPath = `${LocalReturnData.ItemNameAsFolderPath}/${LocalImagesPath}`;
+    LocalReturnData.RowPkAsFolderPath = `${LocalReturnData.ImagesFolderPath}/${LocalRowPk}`
+
+    try {
+        fs.mkdirSync(LocalReturnData.RowPkAsFolderPath, {
+            recursive: true
+        });
+
+        LocalReturnData.KTF = true;
+    } catch (error) {
+        LocalReturnData.KReason = "Images Folder not found!";
+    };
+
+    return LocalReturnData;
+};
+
+if (CommonMock.AllowMock) {
+    if (CommonMock.MockKey === 'K1333') {
+        let LocalMockData = require('./Create.json');
+
+        let LocalData = StartFunc({
+            inDataPK: CommonMock.DataPK,
+            ...LocalMockData
+        });
+        console.log('LocalData : ', LocalData);
+
+    };
+};
+
+module.exports = { StartFunc };
