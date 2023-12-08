486a9569a85707fa9c1d74d5b3698dc4a51d4445
Keshav <nknnkumar@yahoo.com>
2023-12-08 13:33:39 +0530

    keshav

--- DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/AutoPost.js
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/AutoPost.js
@@ -7,17 +7,17 @@ let StartFunc = async ({ inClientName, inPurchasePK, inFileNameOnly, inDataPk })
     switch (inClientName) {
         case "Maguva":
             let LocalFromMaguva = await CommonMaguva.StartFunc({ inPurchasePK, inDataPk });
-            console.log("ddddddddddd : ", LocalFromMaguva);
+            
             return await LocalFromMaguva;
             break;
         case "Washtex":
             let LocalFromWashtex = await CommonWashtex.StartFunc({ inPurchasePK, inDataPk });
-            console.log("ddddddddddd : ", LocalFromWashtex);
+            
             return await LocalFromWashtex;
             break;
         case "Cleaning":
             let LocalFromCleaning = await CommonCleaning.StartFunc({ inPurchasePK, inFileNameOnly, inDataPk });
-            console.log("ddddddddddd : ", LocalFromCleaning);
+            
             return await LocalFromCleaning;
             break;
         default:
--- DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/Clients/Maguva/EntryFile.js
+++ DataSupply/Fs/Config/JSONFolder/DataPkAsFolder/DataFolder/CustomFuncs/Clients/Maguva/EntryFile.js
@@ -16,7 +16,7 @@ let StartFunc = async ({ inPurchasePK, inDataPk }) => {
     };
 
     LocalReturnObject.KTF = false;
-    
+
     if (LocalCheckFunc.KTF === false) {
         return LocalReturnObject;
     };
@@ -32,7 +32,78 @@ let StartFunc = async ({ inPurchasePK, inDataPk }) => {
 
     async.forEachOf(LocalPurchasePK.InvGrid, (InvGridvalue, InvGridkey) => {
         if ("Qty" in InvGridvalue) {
+            let LoopInSideCeilingValue = Math.ceil(InvGridvalue.Qty);
 
+            async.times(LoopInSideCeilingValue, (n) => {
+                let LocalFromCommonDataFolderPushData = CommonDataFolderPushData.StartFuncNoAsync({
+                    inFolderName: "QrCodes",
+                    inFileNameOnly: "Generate",
+                    inItemName: "Barcodes",
+                    inDataPK: localDatapk,
+                    inDataToInsert: {
+                        CostPrice: InvGridvalue.UnitRate,
+                        ProductName: InvGridvalue.ItemName,
+                        ProductAliasName: "",
+                        SalePrice: InvGridvalue.MRP,
+                        PercentageValueAddition: InvGridvalue.PercentageValueAddition,
+                        UserDescription: `${LocalAliasName}-${inPurchasePK}-${InvGridkey}-${InvGridvalue.Qty}`,
+                        InventorySerial: InvGridkey,
+                        PurchasePk: inPurchasePK,
+                        SupplierName: LocalSupplierName,
+                        BillNumber: LocalBillNumber,
+                    }
+                });
+
+                LocalReturnObject.KResult.push(LocalFromCommonDataFolderPushData);
+            }, err => {
+                if (err) {
+                    console.log(err);
+                    // return console.log(err);
+                };
+            });
+        };
+    }, err => {
+        if (err) console.error(err.message);
+    });
+
+    let QrCodesAfter = LocalAfterPost({ inDataPk: localDatapk });
+
+    LocalReturnObject.KTF = true;
+    // LocalReturnObject.QrCodesRaised = parseInt(parseInt(QrCodesAfter) - parseInt(QrCodesBefore));
+
+    LocalReturnObject.QrCodesRaised = QrCodesAfter - QrCodesBefore;
+    delete LocalReturnObject.PurchasePk;
+    delete LocalReturnObject.KResult;
+
+    return await LocalReturnObject;
+};
+
+
+let LocalFuncForLoop_CanBeDeleted = async ({ inPurchasePK, inDataPk }) => {
+    let localDatapk = inDataPk;
+    let LocalCheckFunc = CommonCheckFunc.StartFunc({ inPurchasePK, inDataPk });
+    let LocalReturnObject = {
+        ...LocalCheckFunc
+    };
+
+    LocalReturnObject.KTF = false;
+
+    if (LocalCheckFunc.KTF === false) {
+        return LocalReturnObject;
+    };
+
+    // Object.seal(LocalReturnObject);
+
+    let LocalPurchasePK = LocalReturnObject.PurchasePk;
+    let LocalSupplierName = LocalPurchasePK.SupplierName;
+    let LocalBillNumber = LocalPurchasePK.BillNumber;
+    let LocalAliasName = LocalPurchasePK.AliasName;
+
+    let QrCodesBefore = LocalBeforePost({ inDataPk: localDatapk });
+
+    async.forEachOf(LocalPurchasePK.InvGrid, (InvGridvalue, InvGridkey) => {
+        if ("Qty" in InvGridvalue) {
+            console.log("InvGridvalue.Qty : ", InvGridvalue.Qty);
             async.times(InvGridvalue.Qty, (n) => {
                 let LocalFromCommonDataFolderPushData = CommonDataFolderPushData.StartFuncNoAsync({
                     inFolderName: "QrCodes",
@@ -91,15 +162,15 @@ let LocalAfterPost = ({ inDataPk }) => {
 };
 
 if (CommonMock.AllowMock) {
-    if (CommonMock.MockKey === 'hello') {
+    if (CommonMock.MockKey === 'M08') {
         let LocalMockData = require('./EntryFile.json');
 
-        let Output = StartFunc({
+        StartFunc({
             inDataPk: CommonMock.DataPK,
             ...LocalMockData
+        }).then(PromiseData => {
+            console.log('PromiseData : ', PromiseData);
         });
-        // console.log('Output : ', Output);
-
     };
 };
 
